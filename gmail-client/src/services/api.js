db15a835b865626d48b48f489829ed2e947dbe48
manish.kumar@borderfree.io-at-505103238211 <manish.kumar@borderfree.io>
2023-09-22 10:55:44 +0530

    api change

--- gmail-client/src/services/api.js
+++ gmail-client/src/services/api.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const API_GMAIL = async (urlObjects,payload,type) => {
   console.log('payload: api.js ', payload);
   console.log('urlObjects: api.js ', urlObjects);
-  const API_URL = "https://gmail-server-7mzp.onrender.com";
+  const API_URL = "https://gmailserver.onrender.com";
 
   return await axios({
     method: urlObjects.method,
